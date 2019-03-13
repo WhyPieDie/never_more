4b9a0fb4987037fd8c9083b0434dfa370097c16f
WhyPieDie <dmytro.butrynskyy+github@gmail.com>
2019-03-13 15:17:57 +0200

    Added config; Added handlers for 'never' messages.

--- main.js
+++ main.js
@@ -1,29 +1,43 @@
 const Telegraf = require('telegraf');
-const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+const Fs = require('fs');
+
+const config = require('config.json');
+const mongo_connection_string = config.dev.db_connection_string;
 
 bot = new Telegraf(process.env.BOT_TOKEN);
 
-//bot.use((context, next) => {console.log(context); return next(context)});
-bot.start(context => context.reply('Хей хо!'));
+bot.use((context, next) => {
+    return next(context)
+        .then(context => console.log(context))
+});
+bot.start(context => context.reply('Шалом!'));
 
-bot.help(context => context.reply('хелпи не буде'));
+bot.help(context => context.reply('хелпи не буде!'));
 
+const phrase = 'я ніколи не '.toLowerCase();
 
-bot.on('message', (context, next) => {
-    return next(context).then(() => {
-    //context.reply(context.message.text + 'asdasd');
-})
+bot.on('message', context => {
+    if(context.message.text.toLowerCase().startsWith(phrase)) {
+        let action = context.message.text.substring(phrase.length);
+        if(action.length > 0) {
+            context.reply('added: ' + action);
+        }
+    }
 });
 
-bot.command('new', (context, next) => {
-    return next(context).then(() => {
+bot.command('new', context => {
     console.log(context.message.entities);
     let bot_commands_length = context.message.entities.slice(-1)[0].offset;
-    console.log(bot_commands_length);
     let message_text = context.message.text.slice(bot_commands_length);
     console.log(message_text);
-    context.reply('really? ' + message_text);
-    });
+    message_text = message_text.toLowerCase();
+    if(message_text.startsWith(phrase)) {
+        message_text = message_text.substring(phrase.length);
+    }
+    if(message_text.length > 0) {
+        context.reply('new: ' + message_text);
+    }
 })
 
 bot.command('wtf', context => {
@@ -32,7 +46,7 @@ bot.command('wtf', context => {
 })
 
 
-let launch = bot.launch((ctx) => ctx.reply('asdasdasdasda'));
+let launch = bot.launch((ctx) => ctx.reply('running...'));
 bot.command('stop', (context) => {
     console.log(launch);
     launch.then(() => {
@@ -43,6 +57,6 @@ bot.command('stop', (context) => {
 })})
 
 bot.command('pasta', (context) => {
-    let file = fs.readFileSync('./ubludok.txt', 'utf8');
+    let file = Fs.readFileSync('./ubludok.txt', 'utf8');
     context.reply(file);
 })

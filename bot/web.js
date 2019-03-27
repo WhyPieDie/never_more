67604b27813bd7523652b1f721991827ed4af05e
WhyPieDie <dmytro.butrynskyy+github@gmail.com>
2019-03-27 16:39:28 +0200

    added GET/phrases

--- bot/web.js
+++ bot/web.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const packageInfo = require('./package.json');
+const config = require('./config.json');
+const db = require('./db');
 
 
 const app = express();
@@ -10,7 +12,17 @@ app.get('/', function (req, res) {
   res.json({ version: packageInfo.version });
 });
 
-var server = app.listen(process.env.PORT, "0.0.0.0", () => {
+app.get('/phrases', async function (req, res) {
+  let db_uri = config.db_uri_template
+    .replace('$user', config.db_user)
+    .replace('$password', config.db_password);
+  let database = new db(db_uri, config.db_name, config.db_collection);
+  let collection = await database.get_collection();
+  let phrases = await database.get_phrases(collection)
+  res.json( {phrases : phrases});
+})
+
+var server = app.listen(/*process.env.PORT*/ 43240, "0.0.0.0", () => {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Web server started at http://%s:%s', host, port);

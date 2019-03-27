51736a4fbee6aa0a1cb0c5e8176109c76977797c
WhyPieDie <dmytro.butrynskyy+github@gmail.com>
2019-03-27 16:40:14 +0200

    fixed crash because of default https

--- bot/index.js
+++ bot/index.js
@@ -7,5 +7,5 @@ var bot = require('./bot');
 require('./web')(bot);
 
 setInterval(() => {
-    http.get(config.app_url)
+    http.get(config.app_url, {protocol: 'http:'}, response => console.log(`keep alive cycle : ${response.statusCode}`));
 }, 5 * 60 * 1000);

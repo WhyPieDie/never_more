ead4073e57497038e125e29d080cf03a2b07ea35
Dmytro Butrynskyy <dmytro.butrynskyy+github@gmail.com>
2019-03-27 11:36:06 +0200

    added polling to keep heroku host alive

--- bot/index.js
+++ bot/index.js
@@ -1,4 +1,11 @@
 require("dotenv").config();
+const http = require('http');
+const config = require('./config.json');
+
 
 var bot = require('./bot');
 require('./web')(bot);
+
+setInterval(() => {
+    http.get(config.app_url)
+}, 5 * 60 * 1000);
